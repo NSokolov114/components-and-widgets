c1686b8e8a937d35fd48e22ad77c5674aae01afb
NSokolov114 <sokoloff114@gmail.com>
2021-03-16 16:52:30 +0300

    feat: rnd choice picker

--- rnd-choice-picker/script.js
+++ rnd-choice-picker/script.js
@@ -1,90 +1,63 @@
-// function declaration
-function newFunction() {
-    //...
-}
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
 
-// function expression
-var newFunction = function() {
-};
+textarea.focus()
 
-// expression
-1+3;
-var a = 2;
-return true;
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
 
-// calling (invoking) a function
-alert();
-newFunction(param1, param2);
-
-// assign a variable
-var a = true;
-
-// function vs method
-function thisIsAFunction() {
-
-}
-var obj = {
-    thisIsAMethod: function() {
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+        randomSelect()
     }
-}
-
-thisIsAFunction();
-obj.thisIsAMethod();
-
-
-// to do list
-var todos = [
-    "clean room",
-    "brush teeth",
-    "exercise",
-    "study javascript",
-    "eat healthy"
-];
-
-for (var i=0; i < todos.length; i++) {
-    console.log(todos[i]);
-    todos[i] = todos[i] + "!";
-    todos.pop();
-}
-
-todos.forEach(function(i) {
-    console.log(i);
 })
 
-var counterOne = 0;
-while (counterOne < 10) {
-    console.log(counterOne);
-    counterOne++;
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    });
 }
 
-var counterTwo = 10;
-do {
-    console.log(counterTwo);
-    counterTwo--;
-} while (counterTwo > 0);
+function randomSelect() {
+    const times = 30
 
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
 
+        highlightTag(randomTag)
 
-// function sayHello() {
-//     console.log("Hello");
-// }
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        },100)
+    }, 100)
 
-// sayHello();
+    setTimeout(() => {
+        clearInterval(interval)
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+        }, 100);
+    }, times * 100);
+}
 
-// var sayBye = function() {
-//     console.log("Bye");
-// }
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
 
-// sayBye();
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
 
-// function multiply(a, b) {
-//     if (a > 10 || b > 10) {
-//         return "That's too hard!";
-//     } else {
-//         return a * b;
-//     }
-// }
-
-// multiply(5, 10);
-
-// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
