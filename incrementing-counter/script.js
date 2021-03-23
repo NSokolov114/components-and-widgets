62afe32d3aa7de3eff034d4c78900bad71bbd6f7
NSokolov114 <sokoloff114@gmail.com>
2021-03-23 16:58:41 +0300

    feat: add working counter

--- incrementing-counter/script.js
+++ incrementing-counter/script.js
@@ -1,90 +1,18 @@
-// function declaration
-function newFunction() {
-    //...
-}
+const counters = document.querySelectorAll('.counter')
 
-// function expression
-var newFunction = function() {
-};
+counters.forEach(counter => {
+    counter.innerText = '0'
 
-// expression
-1+3;
-var a = 2;
-return true;
-
-// calling (invoking) a function
-alert();
-newFunction(param1, param2);
-
-// assign a variable
-var a = true;
-
-// function vs method
-function thisIsAFunction() {
-
-}
-var obj = {
-    thisIsAMethod: function() {
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+        const increment = (target / 79)
+        if (c<target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
     }
-}
-
-thisIsAFunction();
-obj.thisIsAMethod();
-
-
-// to do list
-var todos = [
-    "clean room",
-    "brush teeth",
-    "exercise",
-    "study javascript",
-    "eat healthy"
-];
-
-for (var i=0; i < todos.length; i++) {
-    console.log(todos[i]);
-    todos[i] = todos[i] + "!";
-    todos.pop();
-}
-
-todos.forEach(function(i) {
-    console.log(i);
+    updateCounter()
 })
-
-var counterOne = 0;
-while (counterOne < 10) {
-    console.log(counterOne);
-    counterOne++;
-}
-
-var counterTwo = 10;
-do {
-    console.log(counterTwo);
-    counterTwo--;
-} while (counterTwo > 0);
-
-
-
-// function sayHello() {
-//     console.log("Hello");
-// }
-
-// sayHello();
-
-// var sayBye = function() {
-//     console.log("Bye");
-// }
-
-// sayBye();
-
-// function multiply(a, b) {
-//     if (a > 10 || b > 10) {
-//         return "That's too hard!";
-//     } else {
-//         return a * b;
-//     }
-// }
-
-// multiply(5, 10);
-
-// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
