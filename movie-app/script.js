648c72da3f1f8fd4d75053af47a15a394170de11
NSokolov114 <sokoloff114@gmail.com>
2021-04-09 15:15:04 +0300

    add js for the movie app project

--- movie-app/script.js
+++ movie-app/script.js
@@ -4,18 +4,51 @@ const SEARCH_API = 'https://api.themoviedb.org/4/search/movie?api_key=55ce14f036
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+const main = document.getElementById('main')
 //get initial movies
 // failing to fetch
 getMovies(API_URL)
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
+    
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+    movies.forEach((movie) => {
+        const { title,poster_path,vote_average,overview } = movie
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+        
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                <p>${overview}</p>
+            </div>
+        
+        `
+        main.appendChild(movieEl)
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green'
+    } else if (vote >= 5.5) {
+        return 'orange'
+    } else {
+        return 'red'
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-
     const searchTerm = search
     if (searchTerm && searchTerm !== '') {
         getMovies(SEARCH_API + searchTerm)
@@ -24,21 +57,3 @@ form.addEventListener('submit', (e) => {
         window.location.reload()
     }
 })
-
-// const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
-// const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-// const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
-
-// const main = document.getElementById('main')
-// const form = document.getElementById('form')
-// const search = document.getElementById('search')
-
-// // Get initial movies
-// getMovies(API_URL)
-
-// async function getMovies(url) {
-//     const res = await fetch(url)
-//     const data = await res.json()
-
-//     showMovies(data.results)
-// }
