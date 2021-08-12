458331f1dab7ebe6889840a424fdf45203e04378
NSokolov114 <sokoloff114@gmail.com>
2021-08-12 23:26:20 +0300

    add js for password strength background project

--- password-strength-background/script.js
+++ password-strength-background/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const background = document.getElementById('background');
+const password = document.getElementById('password');
+
+password.addEventListener('input', () => {
+  background.style.filter = `blur(${16 - password.value.length * 2}px)`;
+  if (password.value.length > 7)
+    password.style.background = 'rgba(57, 168, 168, 0.3)';
+});
