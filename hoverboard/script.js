a75c27e6d0b76c64b83cae300373849ba97a6919
NSokolov114 <sokoloff114@gmail.com>
2021-08-16 20:59:05 +0300

    add js for hoverboard

--- hoverboard/script.js
+++ hoverboard/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const container = document.querySelector('.container'),
+  colors = [
+    'red',
+    'blue',
+    'green',
+    'orange',
+    'yellow',
+    'white',
+    'purple',
+    'black',
+    'turquoise',
+  ],
+  SQUARES = 500,
+  defaultColor = 'rgb(39, 39, 39)';
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  container.appendChild(square);
+  square.addEventListener('mouseover', () => {
+    setColor(square);
+  });
+  square.addEventListener('mouseout', () => {
+    removeColor(square);
+  });
+}
+
+function setColor(e) {
+  const color = getRandomColor();
+  e.style.background = color;
+  e.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(e) {
+  e.style.background = defaultColor;
+  e.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor(e) {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
