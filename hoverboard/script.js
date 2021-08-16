ac52f00b51777344dfb277b52c3b419b508ce2ae
NSokolov114 <sokoloff114@gmail.com>
2021-08-16 21:15:04 +0300

    feat 16kk colors for hoverboard

--- hoverboard/script.js
+++ hoverboard/script.js
@@ -1,43 +1,37 @@
 'use strict';
 
-const container = document.querySelector('.container'),
-  colors = [
-    'red',
-    'blue',
-    'green',
-    'orange',
-    'yellow',
-    'white',
-    'purple',
-    'black',
-    'turquoise',
-  ],
-  SQUARES = 500,
-  defaultColor = 'rgb(39, 39, 39)';
+document.addEventListener('DOMContentLoaded', ready);
+function ready() {
+  const container = document.querySelector('.container'),
+    SQUARES = 595,
+    defaultColor = 'rgb(39, 39, 39)';
 
-for (let i = 0; i < SQUARES; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
-  square.addEventListener('mouseover', () => {
-    setColor(square);
-  });
-  square.addEventListener('mouseout', () => {
-    removeColor(square);
-  });
-}
+  for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover', () => {
+      setColor(square);
+    });
+    square.addEventListener('mouseout', () => {
+      removeColor(square);
+    });
+    container.appendChild(square);
+  }
 
-function setColor(e) {
-  const color = getRandomColor();
-  e.style.background = color;
-  e.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-}
+  function setColor(e) {
+    const color = getRandomColor();
+    e.style.background = color;
+    e.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+  }
 
-function removeColor(e) {
-  e.style.background = defaultColor;
-  e.style.boxShadow = `0 0 2px #000`;
-}
+  function removeColor(e) {
+    e.style.background = defaultColor;
+    e.style.boxShadow = `0 0 2px #000`;
+  }
 
-function getRandomColor(e) {
-  return colors[Math.floor(Math.random() * colors.length)];
+  function getRandomColor(e) {
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`;
+  }
 }
