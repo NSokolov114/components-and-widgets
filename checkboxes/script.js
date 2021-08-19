4c08fe97395910ef4221691662297568ee1e0eb7
NSokolov114 <sokoloff114@gmail.com>
2021-08-19 21:40:08 +0300

    add css js for fun checkboxes

--- checkboxes/script.js
+++ checkboxes/script.js
@@ -1,4 +1,25 @@
 'use strict';
 
-// document.addEventListener('DOMContentLoaded', ready);
-// function ready() {}
+document.addEventListener('DOMContentLoaded', ready);
+function ready() {
+  const toggles = document.querySelectorAll('.toggle'),
+    good = document.querySelector('#good'),
+    fast = document.querySelector('#fast'),
+    cheap = document.querySelector('#cheap');
+
+  toggles.forEach(toggle =>
+    toggle.addEventListener('change', e => doTheTrick(e.target))
+  );
+
+  function doTheTrick(theClickedOne) {
+    if (good.checked && fast.checked && cheap.checked) {
+      if (good === theClickedOne) {
+        fast.checked = false;
+      } else if (cheap === theClickedOne) {
+        good.checked = false;
+      } else if (fast === theClickedOne) {
+        cheap.checked = false;
+      }
+    }
+  }
+}
