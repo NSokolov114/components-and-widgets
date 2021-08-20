0b848d8ae24f96538c217b4e446a2fe0007b8944
NSokolov114 <sokoloff114@gmail.com>
2021-08-20 20:11:25 +0300

    add js for mobile tab navigation project

--- mobile-tab-navigation/script.js
+++ mobile-tab-navigation/script.js
@@ -1,4 +1,20 @@
 'use strict';
 
-// document.addEventListener('DOMContentLoaded', ready);
-// function ready() {}
+document.addEventListener('DOMContentLoaded', ready);
+function ready() {
+  const images = document.querySelectorAll('.content');
+  const menuItems = document.querySelectorAll('li');
+
+  for (let i = 0; i < images.length; i++) {
+    menuItems[i].addEventListener('click', () => {
+      for (let j = 0; j < images.length; j++) {
+        if (i !== j) {
+          images[j].classList.remove('show');
+          menuItems[j].classList.remove('active');
+        }
+      }
+      images[i].classList.add('show');
+      menuItems[i].classList.add('active');
+    });
+  }
+}
