859a2f6c1c8fe56032088523e6b9162db793ed94
NSokolov114 <sokoloff114@gmail.com>
2021-09-06 20:55:44 +0300

    feat generating random list via JS drag-n-drop sorted list with JS project

--- drag-and-drop-list/script.js
+++ drag-and-drop-list/script.js
@@ -1,4 +1,46 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', ready);
-function ready() {}
+function ready() {
+  const draggable_list = document.getElementById('draggable-list'),
+    check = document.getElementById('check');
+
+  const richestPeople = [
+    'Jeff Bezos',
+    'Bill Gates',
+    'Warren Buffett',
+    'Bernard Arnault',
+    'Carlos Slim Helu',
+    'Amancio Ortega',
+    'Larry Ellison',
+    'Mark Zuckerberg',
+    'Michael Bloomberg',
+    'Larry Page',
+  ];
+
+  const listItems = []; // for storing list items
+
+  let dragStartIndex;
+
+  createList();
+
+  function createList() {
+    //insert list items into DOM
+    [...richestPeople]
+      .map(a => ({ value: a, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(a => a.value)
+      .forEach((person, index) => {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('data-index', index);
+        listItem.innerHTML = `<span class="number">${index + 1}</span>
+  <div class="draggable" draggable="true">
+    <p class="person-name">${person}</p>
+    <i class="fas fa-grip-lines"></i>
+  </div>`;
+        // console.log(listItem);
+        listItems.push(listItem);
+        draggable_list.appendChild(listItem);
+      });
+  }
+}
