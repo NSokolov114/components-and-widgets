17810637ff3ae013e599a001b988a3d447afc9df
NSokolov114 <sokoloff114@gmail.com>
2021-09-09 22:24:38 +0300

    feat random image feed project

--- rnd-image-feed/script.js
+++ rnd-image-feed/script.js
@@ -1,4 +1,21 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', ready);
-function ready() {}
+function ready() {
+  const container = document.querySelector('.container');
+
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  const x = Math.round(width / 500);
+  const y = Math.round(height / 500);
+
+  for (let i = 0; i < x * y; i++) {
+    const newImg = document.createElement('img');
+    newImg.src = `https://source.unsplash.com/user/nsokolov114/${
+      width / x + i
+    }x${height / y}`;
+    newImg.style.width = `${100 / x}%`;
+    container.appendChild(newImg);
+  }
+}
