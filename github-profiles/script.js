b2c62afad0b6771b1e8d0ec54076119f4a2648c4
NSokolov114 <sokoloff114@gmail.com>
2021-10-19 21:12:46 +0300

    feat axios for the github profiles

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -1,4 +1,31 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', ready);
-function ready() {}
+function ready() {
+  const APIURL = 'https://api.github.com/users/';
+  const form = document.getElementById('form');
+  const search = document.getElementById('search');
+
+  getUser('nsokolov114');
+
+  async function getUser(username) {
+    try {
+      const { data } = await axios(APIURL + username);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+      getUser(user);
+
+      search.value = '';
+    }
+  });
+}
