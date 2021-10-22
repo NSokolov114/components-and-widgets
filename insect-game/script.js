cf612cf0fbd69a0f2489019ac2a9683e9eb06d45
NSokolov114 <sokoloff114@gmail.com>
2021-10-22 20:26:56 +0300

    add html css for insect catch game

--- insect-game/script.js
+++ insect-game/script.js
@@ -1,4 +1,8 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', ready);
-function ready() {}
+function ready() {
+  const screens = document.querySelectorAll('.screen');
+
+  screens[0].classList.add('up');
+}
