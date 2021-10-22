9387c92ecd89fa8adf29bd64cb088a6e6e0a12c5
NSokolov114 <sokoloff114@gmail.com>
2021-10-22 20:01:51 +0300

    add js for feedback UI project

--- feedback-ui/script.js
+++ feedback-ui/script.js
@@ -1,4 +1,35 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', ready);
-function ready() {}
+function ready() {
+  const ratings = document.querySelectorAll('.rating');
+  const ratingsContainer = document.querySelector('.rating-container');
+  const sendBtn = document.querySelector('#send');
+  const panel = document.querySelector('#panel');
+  let selectedRating = 'Satisfied';
+
+  ratingsContainer.addEventListener('click', e => {
+    if (e.target.parentNode.classList.contains('rating')) {
+      removeActive();
+      e.target.parentNode.classList.add('active');
+      selectedRating = e.target.nextElementSibling.innerHTML;
+    }
+  });
+
+  sendBtn.addEventListener('click', e => {
+    panel.innerHTML = `
+  <i class="fas fa-heart"></i>
+  <strong>Thank You!</strong>
+  <br>
+  <strong>Feedback: ${selectedRating}</strong>
+  <br>
+  <p>We'll use your feedback to improve our customer support</p>
+  `;
+  });
+
+  function removeActive() {
+    for (let i = 0; i < ratings.length; i++) {
+      ratings[i].classList.remove('active');
+    }
+  }
+}
