d5a0548626f8eb602da6d90fded7abca04d0a03e
NSokolov114 <sokoloff114@gmail.com>
2021-10-25 18:23:30 +0300

    fix css and add js for the netflix navigation

--- netflix-navigation/script.js
+++ netflix-navigation/script.js
@@ -1,4 +1,20 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', ready);
-function ready() {}
+function ready() {
+  const open_btn = document.querySelector('.open-btn'),
+    close_btn = document.querySelector('.close-btn'),
+    nav = document.querySelectorAll('.nav');
+
+  open_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => {
+      nav_el.classList.add('visible');
+    });
+  });
+
+  close_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => {
+      nav_el.classList.remove('visible');
+    });
+  });
+}
