9c6f5d1e802228f6a7923f1967e09fc465e00001
NSokolov114 <sokoloff114@gmail.com>
2021-10-29 20:07:10 +0300

    refactor scroll animation

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -1,18 +1,20 @@
-const boxes = document.querySelectorAll('.box')
+'use strict';
 
-window.addEventListener('scroll', checkBoxes)
-//run once to show before scroll
-checkBoxes()
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+checkBoxes();
 
 function checkBoxes() {
-    const triggerBottom = window.innerHeight * 0.85
+  const triggerBottom = window.innerHeight * 0.85;
 
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top
-        if (boxTop < triggerBottom) {
-            box.classList.add('show')
-        } else {
-            box.classList.remove('show')
-        }
-    })
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
 }
