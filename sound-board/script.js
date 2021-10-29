73aed87c9f4705030548553f802522dec8e055f2
NSokolov114 <sokoloff114@gmail.com>
2021-10-29 20:02:42 +0300

    refactor sound board

--- sound-board/script.js
+++ sound-board/script.js
@@ -1,25 +1,25 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+'use strict';
+
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
 sounds.forEach(sound => {
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
+  const btn = document.createElement('button');
 
-    btn.innerText = sound
+  btn.classList.add('btn');
+  btn.innerText = sound;
+  btn.addEventListener('click', () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
 
-    btn.addEventListener('click', () => {
-        stopSongs()
-
-        document.getElementById(sound).play()
-    })
-
-    document.getElementById('buttons').appendChild(btn)
-})
+  document.getElementById('buttons').appendChild(btn);
+});
 
 function stopSongs() {
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound)
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound);
 
-        song.pause()
-        song.currentTime = 0;
-    })
+    song.pause();
+    song.currentTime = 0;
+  });
 }
