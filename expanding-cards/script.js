061aaa615f6e6879de92df2eba538c95adcbd08e
NSokolov114 <sokoloff114@gmail.com>
2021-11-03 19:12:04 +0300

    refactor expanding cards

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,21 +1,17 @@
+'use strict';
 
-const panels = document.querySelectorAll('.panel')
-// returns all panels in an array (panels[0], ...)
+const panels = document.querySelectorAll('.panel');
 
-panels.forEach((panel) => {
-    // console.log(panel) - loop through all panels
-    panel.addEventListener('click', () => {
-        // console.log('123');
-        removeActiveClasses();
-        // function - removes active classes
-        panel.classList.add('active'); 
-        // adds class active on click
-    })
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
 
-})
+    panel.classList.add('active');
+  });
+});
 
 function removeActiveClasses() {
-    panels.forEach((panel) => {
-        panel.classList.remove('active');
-    })
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
 }
