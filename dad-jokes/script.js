eabcaa7a5de91aeea639a0bc33861f97cbf269d5
NSokolov114 <sokoloff114@gmail.com>
2021-11-04 22:51:20 +0300

    refactor dad jokes app

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -1,32 +1,19 @@
+'use strict';
 
-const jokeEl = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
+const jokeEl = document.getElementById('joke'),
+  jokeBtn = document.getElementById('jokeBtn');
 
-jokeBtn.addEventListener('click', generateJoke)
+jokeBtn.addEventListener('click', generateJoke);
 
-generateJoke()
-// 1 - ASYNC/AWAIT:
+generateJoke();
+
 async function generateJoke() {
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        }
-    }
-    const response = await fetch('https://icanhazdadjoke.com', config)
-    const data = await response.json()
-    jokeEl.innerHTML = data.joke
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  const response = await fetch('https://icanhazdadjoke.com', config);
+  const data = await response.json();
+  jokeEl.innerHTML = data.joke;
 }
-
-// 2 - .then:
-// function generateJoke() {
-//     const config = {
-//         headers: {
-//             'Accept': 'application/json'
-//         }
-//     }
-//     fetch('https://icanhazdadjoke.com', config)
-//     .then(response => response.json())
-//     .then(data => {
-//         jokeEl.innerHTML = data.joke
-//     })
-// }
