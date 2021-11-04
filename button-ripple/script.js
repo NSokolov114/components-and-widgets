cb5499e4b065810ff3971c880a31e16eb5aa8956
NSokolov114 <sokoloff114@gmail.com>
2021-11-04 23:04:17 +0300

    refactor button ripple

--- button-ripple/script.js
+++ button-ripple/script.js
@@ -1,23 +1,26 @@
-const buttons = document.querySelectorAll('.ripple')
+'use strict';
+
+const buttons = document.querySelectorAll('.ripple');
 
 buttons.forEach(button => {
-    button.addEventListener('click', function (e) {
-        const x = e.clientX
-        const y = e.clientY
-        
-        const buttonTop = e.target.offsetTop
-        const buttonLeft = e.target.offsetLeft
-        
-        const xInside = x - buttonLeft
-        const yInside = y - buttonTop
+  button.addEventListener('click', function (e) {
+    const x = e.clientX;
+    const y = e.clientY;
 
-        const circle = document.createElement('span')
-        circle.classList.add('circle')
-        circle.style.top = yInside + 'px'
-        circle.style.left = xInside + 'px'
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
 
-        this.appendChild(circle)
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
 
-        setTimeout(() => circle.remove(), 500)
-    })
-})
+    const circle = document.createElement('span');
+
+    circle.classList.add('circle');
+    circle.style.top = yInside + 'px';
+    circle.style.left = xInside + 'px';
+
+    this.appendChild(circle);
+
+    setTimeout(() => circle.remove(), 500);
+  });
+});
