6eae3f19ea306dc0c198dc68fda35449f75d24e7
NSokolov114 <sokoloff114@gmail.com>
2021-11-05 23:26:42 +0300

    refactor animated navigation

--- animated-navigation/script.js
+++ animated-navigation/script.js
@@ -1,5 +1,6 @@
-const toggle = document.getElementById('toggle')
-const nav = document.getElementById('nav')
+'use strict';
 
-toggle.addEventListener('click', () => 
-    nav.classList.toggle('active'))
+const toggle = document.getElementById('toggle'),
+  nav = document.getElementById('nav');
+
+toggle.addEventListener('click', () => nav.classList.toggle('active'));
