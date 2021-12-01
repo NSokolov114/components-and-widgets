2f8aa5cb11df3be2d2bca8a05dc1cbe07a52d401
NSokolov114 <sokoloff114@gmail.com>
2021-12-01 21:58:53 +0300

    feat fetching imgs for infinite scroll

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -1,4 +1,19 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', ready);
 
-function ready() {}
+function ready() {
+  const count = 20;
+  const apiKey = '';
+  const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+  async function getPhotos() {
+    try {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {}
+  }
+
+  getPhotos();
+}
+// &username=nsokolov114
