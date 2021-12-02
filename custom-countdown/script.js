5eb268984ff396d3094b3d8362bdf2f6a9ce6242
NSokolov114 <sokoloff114@gmail.com>
2021-12-02 23:39:06 +0300

    refactor css and js for custom countdown

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -2,27 +2,27 @@
 document.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
-  const inputContainer = document.querySelector('.input-container');
-  const countdownForm = document.querySelector('.form');
-  const dateEl = document.querySelector('#date-picker');
-  const countdownEl = document.querySelector('.countdown');
-  const countdownElTitle = document.querySelector('.countdown-title');
-  const countdownBtn = document.querySelector('.countdown-button');
-  const timeElements = document.querySelectorAll('span');
-  const completeEl = document.querySelector('.complete');
-  const completeElInfo = document.querySelector('.complete-info');
-  const completeBtn = document.querySelector('.complete-button');
+  const inputContainer = document.querySelector('.input-container'),
+    countdownForm = document.querySelector('.form'),
+    dateEl = document.querySelector('#date-picker'),
+    countdownEl = document.querySelector('.countdown'),
+    countdownElTitle = document.querySelector('.countdown-title'),
+    countdownBtn = document.querySelector('.countdown-button'),
+    timeElements = document.querySelectorAll('span'),
+    completeEl = document.querySelector('.complete'),
+    completeElInfo = document.querySelector('.complete-info'),
+    completeBtn = document.querySelector('.complete-button');
 
-  let countdownTitle = '';
-  let countdownDate = '';
-  let countdownValue = Date;
-  let countdownActive;
-  let savedCountdown;
+  let countdownTitle = '',
+    countdownDate = '',
+    countdownValue = new Date(),
+    countdownActive,
+    savedCountdown;
 
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
   const today = new Date().toISOString().slice(0, 10);
 
@@ -44,6 +44,7 @@ function ready() {
       date: countdownDate,
     };
     localStorage.setItem('countdown', JSON.stringify(savedCountdown));
+
     if (countdownDate === '') {
       alert('Please select a date for the countdown');
     } else {
@@ -54,13 +55,12 @@ function ready() {
 
   function updateDOM() {
     countdownActive = setInterval(() => {
-      const now = new Date().getTime();
-      const distance = countdownValue - now;
-
-      const days = Math.floor(distance / day);
-      const hours = Math.floor((distance % day) / hour);
-      const minutes = Math.floor((distance % hour) / minute);
-      const seconds = Math.floor((distance % minute) / second);
+      const now = new Date().getTime(),
+        distance = countdownValue - now,
+        days = Math.floor(distance / day),
+        hours = Math.floor((distance % day) / hour),
+        minutes = Math.floor((distance % hour) / minute),
+        seconds = Math.floor((distance % minute) / second);
 
       inputContainer.classList.add('hidden');
 
@@ -84,10 +84,10 @@ function ready() {
   function reset() {
     countdownEl.classList.add('hidden');
     inputContainer.classList.remove('hidden');
+    completeEl.classList.add('hidden');
     clearInterval(countdownActive);
     countdownTitle = '';
     countdownDate = '';
-    completeEl.classList.add('hidden');
     localStorage.removeItem('countdown');
   }
 
