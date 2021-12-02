de3c12035c0a14562358b88d1e05769a81dc6989
NSokolov114 <sokoloff114@gmail.com>
2021-12-02 13:09:04 +0300

    feat fetching MY imgs and displaying them for infinite scroll

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -2,18 +2,50 @@
 document.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
+  const imageContainer = document.querySelector('.image-container');
+  const loader = document.querySelector('.loader');
+
+  let photosArray = [];
+
   const count = 20;
-  const apiKey = '';
-  const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+  const p1 = 'W1i5zHnpDxpuvpsZw3tYfOXzrTB2ybH3UUK';
+  const p2 = '9YPOg' + 14;
+  const apiKey = `${p1}-${p2}`;
+  const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&username=nsokolov114`;
 
   async function getPhotos() {
     try {
       const response = await fetch(apiURL);
-      const data = await response.json();
-      console.log(data);
+      photosArray = await response.json();
+      displayPhotos();
     } catch (error) {}
+  }
+
+  function displayPhotos() {
+    photosArray.forEach(photo => {
+      const item = document.createElement('a');
+      setAttributes(item, {
+        href: photo.links.html,
+        target: '_blank',
+      });
+
+      const img = document.createElement('img');
+      setAttributes(img, {
+        src: photo.urls.regular,
+        alt: photo.alt_description,
+        title: photo.alt_description,
+      });
+
+      item.appendChild(img);
+      imageContainer.appendChild(item);
+    });
+  }
+
+  function setAttributes(el, att) {
+    for (const key in att) {
+      el.setAttribute(key, att[key]);
+    }
   }
 
   getPhotos();
 }
-// &username=nsokolov114
