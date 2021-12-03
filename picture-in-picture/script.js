3788a989dd8e63c00cedf7c9d6be5cad0bae4581
NSokolov114 <sokoloff114@gmail.com>
2021-12-03 13:55:24 +0300

    feat picture in picture for picture in picture

--- picture-in-picture/script.js
+++ picture-in-picture/script.js
@@ -1,4 +1,29 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', ready);
 
-function ready() {}
+function ready() {
+  const videoElement = document.querySelector('.video'),
+    button = document.querySelector('.button');
+
+  // screen capture API
+  async function selectMediaStream() {
+    try {
+      const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+
+      videoElement.srcObject = mediaStream;
+      videoElement.onloadedmetadata = () => {
+        videoElement.play();
+      };
+    } catch (error) {
+      console.log(`There's an error: ${error}`);
+    }
+  }
+
+  button.addEventListener('click', async () => {
+    button.disabled = true;
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+  });
+
+  selectMediaStream();
+}
