bd2601674fdf6fd56c7404fc2299aba69b0af906
NSokolov114 <sokoloff114@gmail.com>
2021-12-04 15:23:46 +0300

    refactor animated navigation 2

--- animated-navigation-2/script.js
+++ animated-navigation-2/script.js
@@ -2,20 +2,20 @@
 window.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
-  const menuBars = document.querySelector('.menu-bars');
-  const overlay = document.querySelector('.overlay');
-  const navItems = document.querySelectorAll('.nav-item');
+  const menuBars = document.querySelector('.menu-bars'),
+    overlay = document.querySelector('.overlay'),
+    navItems = document.querySelectorAll('.nav-item');
+
+  function toggleNav() {
+    menuBars.classList.toggle('change');
+    overlay.classList.toggle('slide');
+    navItems.forEach(navItem => {
+      navItem.classList.toggle('slide');
+    });
+  }
 
   menuBars.addEventListener('click', toggleNav);
   navItems.forEach(navItem => {
     navItem.addEventListener('click', toggleNav);
   });
-
-  function toggleNav() {
-    menuBars.classList.toggle('change');
-    overlay.classList.toggle('overlay-slide');
-    navItems.forEach(navItem => {
-      navItem.classList.toggle('overlay-slide');
-    });
-  }
 }
