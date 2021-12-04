fc11685c7355c6304a9a5ed661f357c806d32b52
NSokolov114 <sokoloff114@gmail.com>
2021-12-04 15:19:32 +0300

    feat animated navigation 2

--- animated-navigation-2/script.js
+++ animated-navigation-2/script.js
@@ -1,4 +1,21 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', ready);
 
-function ready() {}
+function ready() {
+  const menuBars = document.querySelector('.menu-bars');
+  const overlay = document.querySelector('.overlay');
+  const navItems = document.querySelectorAll('.nav-item');
+
+  menuBars.addEventListener('click', toggleNav);
+  navItems.forEach(navItem => {
+    navItem.addEventListener('click', toggleNav);
+  });
+
+  function toggleNav() {
+    menuBars.classList.toggle('change');
+    overlay.classList.toggle('overlay-slide');
+    navItems.forEach(navItem => {
+      navItem.classList.toggle('overlay-slide');
+    });
+  }
+}
