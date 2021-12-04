4c2517346e54da24cc5f6f43f79d5a787d4a6dc5
NSokolov114 <sokoloff114@gmail.com>
2021-12-04 18:05:22 +0300

    feat play/pause for music player

--- music-player/script.js
+++ music-player/script.js
@@ -2,4 +2,32 @@
 
 window.addEventListener('DOMContentLoaded', ready);
 
-function ready() {}
+function ready() {
+  const music = document.querySelector('audio');
+  const prevBtn = document.querySelector('.prev');
+  const playBtn = document.querySelector('.play');
+  const nextBtn = document.querySelector('.next');
+
+  let isPlaying = false;
+
+  function playSong() {
+    music.play();
+    isPlaying = true;
+
+    playBtn.classList.replace('fa-play', 'fa-pause');
+  }
+
+  function pauseSong() {
+    music.pause();
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+  }
+
+  playBtn.addEventListener('click', () =>
+    isPlaying ? pauseSong() : playSong()
+  );
+
+  // function playSong() {
+  //   music.play();
+  // }
+}
