977c400159b714aa770b985b701f48be454c145e
NSokolov114 <sokoloff114@gmail.com>
2021-12-05 23:47:25 +0300

    init spock game

---
+++ spock-rock-game/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', ready);
+
+function ready() {
+  const choices = {
+    rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
+    paper: { name: 'Paper', defeats: ['rock', 'spock'] },
+    scissors: { name: 'Scissors', defeats: ['paper', 'lizard'] },
+    lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
+    spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
+  };
+}
