3967fb43383d554bb724579cc71d730695fcd11c
NSokolov114 <sokoloff114@gmail.com>
2022-02-23 12:22:32 +0300

    refactor form-continued js and html for the multipage form

--- multipage-order-form/js/form-continued.js
+++ multipage-order-form/js/form-continued.js
@@ -3,15 +3,15 @@
 window.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
-  const inputs = document.querySelectorAll('.signup-form input');
+  const prevInputsContainer = document.querySelector('.signup-form__previous-inputs');
   const urlSearchParams = new URLSearchParams(window.location.search);
 
   for (const [key, value] of urlSearchParams.entries()) {
-    inputs.forEach(input => {
-      if (input.id.includes(key)) {
-        input.value = value;
-        input.closest('p').classList.add('hidden');
-      }
-    });
+    const input = document.createElement('input');
+
+    input.type = 'hidden';
+    input.name = key;
+    input.value = value;
+    prevInputsContainer.appendChild(input);
   }
 }
